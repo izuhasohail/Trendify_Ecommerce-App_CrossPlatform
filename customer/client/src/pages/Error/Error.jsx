56fda0e44d425773b783a552f991afbe2dce94b6
Zuha <zuhasohail2003@gmail.com>
2024-04-06 20:51:35 +0500

    Trendify Ecommerce App

--- customer/client/src/pages/Error/Error.jsx
+++ customer/client/src/pages/Error/Error.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Error = () => {
   return (
-    <div style={{
+    <div
+      style={{
         // width: "50%", margin:"auto",
-        height:'100vh',
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        flexDirection:'column'
-        
-    }}>
-        <h3>404</h3>
-      <h1>Page Not Found</h1>
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h3>404</h3>
+      <h2>Page not found!</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
